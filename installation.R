040021b35d9cb9f6982faa63a019d7dcefc0df65
Deepaknatural <deepaknatural@gmail.com>
2019-08-31 21:47:59 +0530

    Add files via upload

---
+++ installation.R
@@ -0,0 +1,52 @@
+install.packages("dplyr")
+install.packages("mlbench")
+install.packages("ggplot2")
+install.packages("caret")
+install.packages("dummies")
+install.packages("e1071")
+install.packages("data.table")
+#To calculate accurate precision, recall and ROC
+install.packages("precrec")
+#General purpose statistic package
+install.packages("e1071")
+#For Random Forest
+install.packages("randomForest")
+#For plotting data
+install.packages("rpart")
+#For gradient boosting
+install.packages("gbm")
+#For data visualisation
+install.packages("ggplot2")
+
+#install.packages("psych")
+#For data preparation and cleaning
+install.packages("reshape2")
+
+#install and load package arules
+library(arules) 
+search() 
+unloadNamespace("arules") 
+update.packages("arules") 
+library(arules)
+#install.packages("arules")
+#library(arules)
+#install and load arulesViz
+install.packages("arulesViz")
+library(arulesViz)
+#install and load tidyverse
+install.packages("tidyverse")
+library(tidyverse)
+#install and load readxl
+install.packages("readxl")
+library(readxl)
+#install and load knitr
+install.packages("knitr")
+install.packages("RColorBrewer")
+install.packages("twitteR")
+install.packages("ROAuth")
+install.packages("syuzhet")
+# install.packages("stringi")
+install.packages("tm")
+install.packages("SnowballC")
+install.packages("wordcloud")
+install.packages("ggplot2")
